8c43fd2ee283dd5e7e795df7fea9b98c8005179d
Marco Amadei <Amadei.mar@gmail.com>
2014-01-20 16:29:28 +0000

    keepMorror for big mdb

--- src/main/java/net/ucanaccess/jdbc/DBReference.java
+++ src/main/java/net/ucanaccess/jdbc/DBReference.java
@@ -36,7 +36,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.UUID;
 
-
 import net.ucanaccess.converters.LoadJet;
 import net.ucanaccess.util.Logger;
 
@@ -61,12 +60,13 @@ public class DBReference {
 	private boolean readOnlyFileFormat;
 	private boolean showSchema;
 	private File tempHsql;
+	private File toKeepHsql;
 	private boolean singleConnection;
 	private boolean encryptHSQLDB;
 	private String encryptionKey;
 	private String pwd;
 	private JackcessOpenerInterface jko;
-	private Map<String,String> externalResourcesMapping;
+	private Map<String, String> externalResourcesMapping;
 
 	private class MemoryTimer {
 		private final static int INACTIVITY_TIMEOUT_DEFAULT = 120000;
@@ -131,14 +131,12 @@ public class DBReference {
 			this.inactivityTimeout = inactivityTimeout;
 		}
 	}
-	
-	
 
 	public DBReference(File fl, FileFormat ff, JackcessOpenerInterface jko,
 			final String pwd) throws IOException, SQLException {
 		this.dbFile = fl;
-		this.pwd=pwd;
-		this.jko=jko;
+		this.pwd = pwd;
+		this.jko = jko;
 		this.updateLastModified();
 		Logger.turnOffJackcessLog();
 		if (!fl.exists() && ff != null) {
@@ -154,18 +152,21 @@ public class DBReference {
 			this.dbIO.setLinkResolver(new LinkResolver() {
 				public Database resolveLinkedDatabase(Database linkerDb,
 						String linkeeFileName) throws IOException {
-					File linkeeFile=new File(linkeeFileName);
-					Map<String,String> emr=DBReference.this.externalResourcesMapping;
-					if(!linkeeFile.exists()&&
-						emr!=null&&
-						emr.containsKey(linkeeFile.getAbsolutePath().toLowerCase())
-							){
-						linkeeFile=new File(emr.get(linkeeFile.getAbsolutePath().toLowerCase()));
+					File linkeeFile = new File(linkeeFileName);
+					Map<String, String> emr = DBReference.this.externalResourcesMapping;
+					if (!linkeeFile.exists()
+							&& emr != null
+							&& emr.containsKey(linkeeFile.getAbsolutePath()
+									.toLowerCase())) {
+						linkeeFile = new File(emr.get(linkeeFile
+								.getAbsolutePath().toLowerCase()));
 					}
-					if(!linkeeFile.exists()){
-						Logger.logWarning("External file "+linkeeFile.getAbsolutePath()+" does not exist");
+					if (!linkeeFile.exists()) {
+						Logger.logWarning("External file "
+								+ linkeeFile.getAbsolutePath()
+								+ " does not exist");
 					}
-					Database ldb = open(linkeeFile,pwd);
+					Database ldb = open(linkeeFile, pwd);
 					return ldb;
 				}
 			});
@@ -173,10 +174,25 @@ public class DBReference {
 		}
 	}
 
-	public Database open(File dbfl,String pwd) throws IOException {
+	public Database open(File dbfl, String pwd) throws IOException {
 		Logger.turnOffJackcessLog();
-		return jko.open( dbfl,pwd);
-		
+		return jko.open(dbfl, pwd);
+	}
+
+	boolean loadedFromKeptMirror(Session session) throws UcanaccessSQLException {
+		if (this.toKeepHsql != null && this.toKeepHsql.exists()) {
+			if (this.getLastUpdateHSQLDB() >= this.dbFile.lastModified())
+				return true;
+			else {
+				try {
+					this.closeHSQLDB(session);
+				} catch (Exception e) {
+					throw new UcanaccessSQLException(e);
+				}
+				return false;
+			}
+		}
+		return false;
 	}
 
 	public static boolean addOnReloadRefListener(
@@ -206,29 +222,59 @@ public class DBReference {
 		this.closeHSQLDB(session);
 		this.dbIO.flush();
 		this.dbIO.close();
-		this.dbIO =open(this.dbFile,this.pwd);
+		this.dbIO = open(this.dbFile, this.pwd);
 		this.id = id();
 		new LoadJet(getHSQLDBConnection(session), dbIO).loadDB();
 		return getHSQLDBConnection(session);
 	}
 
+	private File[] getHSQLDBFiles() {
+		if( this.toKeepHsql==null)return new File[]{};
+		File folder = this.toKeepHsql.getParentFile();
+		String name = this.toKeepHsql.getName();
+		return new File[] { new File(folder, name + ".data"),
+				new File(folder, name + ".script"),
+				new File(folder, name + ".properties"),
+				new File(folder, name + ".log"),
+				new File(folder, name + ".lck") ,
+				new File(folder, name + ".lobs")};
+	}
+	
+	private long getLastUpdateHSQLDB(){
+		long lu=0;
+		for (File hsqlF : this.getHSQLDBFiles()) {
+			if(hsqlF.exists()&&hsqlF.lastModified()>lu)
+				lu=hsqlF.lastModified();
+		}
+		if( this.toKeepHsql!=null&&this.toKeepHsql.exists()&&this.toKeepHsql.lastModified()>lu)
+			lu=this.toKeepHsql.lastModified();
+		return lu;
+	}
+
 	private void closeHSQLDB(Session session) throws Exception {
 		finalizeHSQLDB(session);
 		if (!this.inMemory) {
-			File folder = dbFile.getParentFile();
-			File hbase = new File(folder, "Ucanaccess_" + this);
-			for (File hsqlF : hbase.listFiles()) {
-				hsqlF.delete();
+			if (this.toKeepHsql == null) {
+				File folder = dbFile.getParentFile();
+				File hbase = new File(folder, "Ucanaccess_" + this);
+				for (File hsqlF : hbase.listFiles()) {
+					hsqlF.delete();
+				}
+				hbase.delete();
+			} else if(!this.singleConnection){
+				this.toKeepHsql.delete();
+				this.toKeepHsql.createNewFile();
+				for (File hsqlf : this.getHSQLDBFiles()) {
+					if(hsqlf.exists())
+					hsqlf.delete();
+				}
 			}
-			hbase.delete();
 		}
 	}
 
 	public void decrementActiveConnection(Session session) {
 		memoryTimer.decrementActiveConnection(session);
 	}
-	
-	
 
 	private void finalizeHSQLDB(Session session) throws Exception {
 		this.releaseLock();
@@ -275,14 +321,15 @@ public class DBReference {
 			}
 		}
 		conn.setAutoCommit(false);
-		
 		return conn;
 	}
 
 	private String key(String pwd) throws SQLException {
+		Connection conn =null;
+		try{
 		if (this.encryptionKey == null) {
 			String url = "jdbc:hsqldb:mem:" + id + "_tmp";
-			Connection conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(url);
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("CALL  CRYPT_KEY('" + CIPHER_SPEC
 					+ "', null) ");
@@ -290,6 +337,10 @@ public class DBReference {
 			this.encryptionKey = rs.getString(1);
 		}
 		return this.encryptionKey;
+		}finally{
+			if(conn!=null)conn.close();
+		
+		}
 	}
 
 	private String getHsqlUrl(final Session session) throws SQLException {
@@ -298,20 +349,34 @@ public class DBReference {
 				lockMdbFile();
 			}
 			String enc = "";
+			String log = "";
 			if (this.encryptHSQLDB) {
 				enc = ";crypt_key=" + key("AES")
 						+ ";crypt_type=aes;crypt_lobs=true";
 			}
+			if (!this.inMemory) {
+				log = ";hsqldb.log_data=FALSE";
+			}
 			if (!this.inMemory && tempHsql == null) {
-				File folder = dbFile.getParentFile();
-				File hbase = new File(folder, "Ucanaccess_" + toString());
-				hbase.mkdir();
-				this.tempHsql = new File(hbase, this.id);
-				this.tempHsql.createNewFile();
+				if (this.toKeepHsql != null) {
+					if (!this.toKeepHsql.exists()) {
+						this.toKeepHsql.createNewFile();
+					}
+					this.tempHsql = this.toKeepHsql;
+				} else {
+					File folder = dbFile.getParentFile();
+					File hbase = new File(folder, "Ucanaccess_" + toString());
+					hbase.mkdir();
+					this.tempHsql = new File(hbase, this.id);
+					this.tempHsql.createNewFile();
+				}
 				Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
 					public void run() {
 						try {
-							closeHSQLDB(session);
+							if (toKeepHsql == null)
+								closeHSQLDB(session);
+							else
+								finalizeHSQLDB(session);
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
@@ -320,7 +385,7 @@ public class DBReference {
 			}
 			return "jdbc:hsqldb:"
 					+ (this.inMemory ? "mem:" + id : tempHsql.getAbsolutePath())
-					+ enc;
+					+ enc + log;
 		} catch (IOException e) {
 			throw new UcanaccessSQLException(e);
 		}
@@ -383,7 +448,7 @@ public class DBReference {
 		for (OnReloadReferenceListener listener : onReloadListeners) {
 			listener.onReload();
 		}
-		this.dbIO = open(dbFile,this.pwd);
+		this.dbIO = open(dbFile, this.pwd);
 	}
 
 	void setDbAccess(Database dbAccess) {
@@ -434,8 +499,20 @@ public class DBReference {
 		this.encryptHSQLDB = encryptHSQLDB;
 	}
 
-	public void setExternalResourcesMapping(Map<String, String> externalResourcesMapping) {
-		this.externalResourcesMapping=externalResourcesMapping;
-		
+	public void setExternalResourcesMapping(
+			Map<String, String> externalResourcesMapping) {
+		this.externalResourcesMapping = externalResourcesMapping;
+	}
+
+	public File getToKeepHsql() {
+		return toKeepHsql;
+	}
+
+	public void setToKeepHsql(File toKeepHsql) {
+		this.toKeepHsql = toKeepHsql;
+	}
+
+	public boolean isEncryptHSQLDB() {
+		return encryptHSQLDB;
 	}
 }
