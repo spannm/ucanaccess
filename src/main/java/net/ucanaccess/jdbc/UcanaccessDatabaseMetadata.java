73c4534c56ff11cc781d0dc462b3f2cb5831a311
Marco Amadei <Amadei.mar@gmail.com>
2014-04-25 09:34:20 +0000

    version

--- src/main/java/net/ucanaccess/jdbc/UcanaccessDatabaseMetadata.java
+++ src/main/java/net/ucanaccess/jdbc/UcanaccessDatabaseMetadata.java
@@ -273,7 +273,7 @@ public class UcanaccessDatabaseMetadata implements DatabaseMetaData {
 	}
 
 	public String getDriverVersion() throws SQLException {
-		return "2.0.0";
+		return "2.0.4.1";
 	}
 
 	public ResultSet getExportedKeys(String catalog, String schema, String table)
