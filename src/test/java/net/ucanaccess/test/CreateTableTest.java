ef56bf945ecaa0745be35e817cbbe6a843dc5a96
Marco Amadei <Amadei.mar@gmail.com>
2014-09-03 21:39:12 +0000

    special characters

--- src/test/java/net/ucanaccess/test/CreateTableTest.java
+++ src/test/java/net/ucanaccess/test/CreateTableTest.java
@@ -43,51 +43,6 @@ public class CreateTableTest extends UcanaccessTestBase {
 		super(accVer);
 	}
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		executeCreateTable(" CREATE \nTABLE AAA ( baaaa \ntext PRIMARY KEY,A long   default 3 not null, C text(255) not null, "
-				+ "d DATETIME default now(), e text default 'l''aria')");
-	}
-
-	public void testCreate() throws Exception {
-		 createSimple();
-		 createPs();
-		 createAsSelect() ;
-		 createAsSelect2();
-		 setTableProperties();
-		setDPK();
-		// defaults();
-	}
-
-	private void createSimple() throws SQLException, IOException {
-		Statement st = null;
-		try {
-			st = super.ucanaccess.createStatement();
-			st.execute("INSERT INTO AAA(baaaa,c) VALUES ('33A','G'   )");
-			st.execute("INSERT INTO AAA(baaaa,a,c) VALUES ('33B',111,'G'   )");
-			Object[][] ver = { { "33A", 3, "G" }, { "33B", 111, "G" } };
-			checkQuery("select baaaa,a,c from AAA order by baaaa", ver);
-		} finally {
-			if (st != null)
-				st.close();
-		}
-	}
-
-	private void createPs() throws SQLException, IOException {
-		PreparedStatement ps = null;
-		try {
-			ps = super.ucanaccess
-					.prepareStatement(" CREATE \nTABLE BBB ( baaaa \nvarchar(2) PRIMARY KEY)");
-			ps.execute(" CREATE TABLE BBB ( baaaa text PRIMARY KEY,b text)");
-			throw new RuntimeException("To block DDL with PreparedStatement");
-		} catch (SQLException ex) {
-			System.out.println("ok");
-		} finally {
-			if (ps != null)
-				ps.close();
-		}
-	}
-
 	private void createAsSelect() throws SQLException, IOException {
 		Statement st = null;
 		try {
@@ -115,16 +70,59 @@ public class CreateTableTest extends UcanaccessTestBase {
 		}
 	}
 
-	public void setTableProperties() throws SQLException {
+	private void createPs() throws SQLException, IOException {
+		PreparedStatement ps = null;
+		try {
+			ps = super.ucanaccess
+					.prepareStatement(" CREATE \nTABLE BBB ( baaaa \nvarchar(2) PRIMARY KEY)");
+			ps.execute(" CREATE TABLE BBB ( baaaa text PRIMARY KEY,b text)");
+			throw new RuntimeException("To block DDL with PreparedStatement");
+		} catch (SQLException ex) {
+			System.out.println("ok");
+		} finally {
+			if (ps != null)
+				ps.close();
+		}
+	}
+
+	private void createSimple() throws SQLException, IOException {
 		Statement st = null;
 		try {
 			st = super.ucanaccess.createStatement();
-			st.execute("create table tbl(c counter  primary key , "
-					+ "number numeric(23,5) default -4.6 not null , "
-					+ "txt1 text(23)  default 'ciao', blank text  default ' ', dt date default date(), txt2 text(33),"
-					+ "txt3 text)");
+			st.execute("INSERT INTO AAA(baaaa,c) VALUES ('33A','G'   )");
+			st.execute("INSERT INTO AAA(baaaa,a,c) VALUES ('33B',111,'G'   )");
+			Object[][] ver = { { "33A", 3, "G" }, { "33B", 111, "G" } };
+			checkQuery("select baaaa,a,c from AAA order by baaaa", ver);
 		} finally {
-			st.close();
+			if (st != null)
+				st.close();
+		}
+	}
+
+	public void defaults() throws Exception {
+		Statement st = null;
+		try {
+			st = super.ucanaccess.createStatement();
+			ResultSet rs = st.executeQuery("SELECT D, E FROM AAA");
+			while (rs.next()) {
+				assertNotNull(rs.getObject(1));
+				assertNotNull(rs.getObject(2));
+			}
+			Database db = ((UcanaccessConnection) super.ucanaccess).getDbIO();
+			Table tb = db.getTable("AAA");
+			PropertyMap pm = tb.getColumn("d").getProperties();
+			assertEquals("now()", pm.getValue(PropertyMap.DEFAULT_VALUE_PROP));
+			PropertyMap pm1 = tb.getColumn("a").getProperties();
+			assertEquals(true, pm1.getValue(PropertyMap.REQUIRED_PROP));
+			tb = db.getTable("TBL");
+			pm = tb.getColumn("NUMBER").getProperties();
+			assertEquals("-4.6", pm.getValue(PropertyMap.DEFAULT_VALUE_PROP));
+			assertEquals(true, pm.getValue(PropertyMap.REQUIRED_PROP));
+			pm = tb.getColumn("BLANK").getProperties();
+			assertEquals(" ", pm.getValue(PropertyMap.DEFAULT_VALUE_PROP));
+		} finally {
+			if (st != null)
+				st.close();
 		}
 	}
 
@@ -153,6 +151,7 @@ public class CreateTableTest extends UcanaccessTestBase {
 			}
 			st = super.ucanaccess.createStatement();
 			st.execute("insert into  dtrx values('Hi all',444.3)");
+			st.execute("insert into  dtrx values('Hi all',4454.3)");
 			dump("select * from dtrx");
 			dump("select * from dunique");
 			super.ucanaccess.commit();
@@ -163,30 +162,46 @@ public class CreateTableTest extends UcanaccessTestBase {
 		}
 	}
 
-	public void defaults() throws Exception {
+	public void setTableProperties() throws SQLException {
 		Statement st = null;
 		try {
 			st = super.ucanaccess.createStatement();
-			ResultSet rs = st.executeQuery("SELECT D, E FROM AAA");
-			while (rs.next()) {
-				assertNotNull(rs.getObject(1));
-				assertNotNull(rs.getObject(2));
-			}
-			Database db = ((UcanaccessConnection) super.ucanaccess).getDbIO();
-			Table tb = db.getTable("AAA");
-			PropertyMap pm = tb.getColumn("d").getProperties();
-			assertEquals("NOW()", pm.getValue(PropertyMap.DEFAULT_VALUE_PROP));
-			PropertyMap pm1 = tb.getColumn("a").getProperties();
-			assertEquals(true, pm1.getValue(PropertyMap.REQUIRED_PROP));
-			tb = db.getTable("TBL");
-			pm = tb.getColumn("NUMBER").getProperties();
-			assertEquals("-4.6", pm.getValue(PropertyMap.DEFAULT_VALUE_PROP));
-			assertEquals(true, pm.getValue(PropertyMap.REQUIRED_PROP));
-			pm = tb.getColumn("BLANK").getProperties();
-			assertEquals(" ", pm.getValue(PropertyMap.DEFAULT_VALUE_PROP));
+			st.execute("create table tbl(c counter  primary key , "
+					+ "number numeric(23,5) default -4.6 not null , "
+					+ "txt1 text(23)  default 'ciao', blank text  default ' ', dt date default date(), txt2 text(33),"
+					+ "txt3 text)");
 		} finally {
-			if (st != null)
-				st.close();
+			st.close();
 		}
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		executeCreateTable(" CREATE \nTABLE AAA ( baaaa \ntext PRIMARY KEY,A long   default 3 not null, C text(255) not null, "
+				+ "d DATETIME default now(), e text default 'l''aria')");
+		
+	}
+
+	public void testCreate() throws Exception {
+		 createSimple();
+		 createPs();
+		 createAsSelect() ;
+		 createAsSelect2();
+		 setTableProperties();
+		setDPK();
+		 defaults();
+	}
+	
+	
+	public void testNaming() throws SQLException{
+		Statement st = super.ucanaccess.createStatement();
+		st.execute(" CREATE TABLE [ggg kk]( [---bgaaf aa] autoincrement PRIMARY KEY, [---bghhaaf b aa] text(222) default 'vvv')");
+		st.execute(" CREATE TABLE [ggg() kk]( [---bgaaf aa] autoincrement PRIMARY KEY, [---bghhaaf b aa] text(222) default 'vvv')");
+		st.execute(" CREATE TABLE [wHere]( [where] autoincrement PRIMARY KEY, [---bghhaaf b aa] text(222) default 'vvv')");
+		st.execute(" drop table  [ggg kk]");
+		st.execute(" CREATE TABLE [ggg kk]( [---bgaaf aa] autoincrement PRIMARY KEY, [---bghhaaf b aa] numeric(22,6) default 12.99)");
+		st.execute(" CREATE TABLE kkk ( [---bgaaf aa] autoincrement PRIMARY KEY, [---bghhaaf b aa] text(222) default 'vvv')");
+		st.execute(" insert into kkk([---bgaaf aa],[---bghhaaf b aa]) values(1,'23fff')");
+		
 	}
 }
