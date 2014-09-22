66120edb295e50a6c39728eda0ab47f3499f8ce7
Marco Amadei <Amadei.mar@gmail.com>
2014-09-22 08:54:33 +0000

    new error message

--- src/main/java/net/ucanaccess/util/Logger.java
+++ src/main/java/net/ucanaccess/util/Logger.java
@@ -34,7 +34,8 @@ public class Logger {
 		UNKNOWN_EXPRESSION,
 		DEFAULT_VALUES_DELIMETERS,
 		USER_AS_COLUMNNAME,
-		ROW_COUNT
+		ROW_COUNT,
+		TRIGGER_UPDATE_CF_ERR
 	}
 	private static PrintWriter logPrintWriter;  
 	private static ResourceBundle messageBundle=ResourceBundle.getBundle("net.ucanaccess.util.messages");
