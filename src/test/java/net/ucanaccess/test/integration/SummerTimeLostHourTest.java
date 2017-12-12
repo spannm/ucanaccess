9e3aee6b8fc79363a1381f8b8b245f17728ccb37
Gord Thompson <gordonthompson@users.sf.net>
2017-12-12 06:36:36 -0700

    improve ticket_5 test

--- src/test/java/net/ucanaccess/test/integration/SummerTimeLostHourTest.java
+++ src/test/java/net/ucanaccess/test/integration/SummerTimeLostHourTest.java
@@ -1,11 +1,34 @@
+/*
+Copyright (c) 2012 Marco Amadei.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package net.ucanaccess.test.integration;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
+
+import com.healthmarketscience.jackcess.CursorBuilder;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Table;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -49,16 +72,37 @@ public class SummerTimeLostHourTest extends AccessVersion2007Test {
     }
 
     @Test
-    public void testForLostHour() throws SQLException {
+    public void testForLostHour() throws SQLException, IOException {
         /*
          * ensure that #2017-03-26 02:00:00# doesn't "magically" 
-         *      become #2017-03-26 01:00:00#
+         *      become #2017-03-26 01:00:00# when written to HSQLDB
          */
         Connection hsqldbConn = ucanaccess.getHSQLDBConnection();
-        Statement s = hsqldbConn.createStatement();
-        ResultSet rs = s.executeQuery("SELECT CAST(DTMFIELD AS VARCHAR(26)) AS str FROM TABLE1 WHERE ID=1");
+        Statement hsqldbStmt = hsqldbConn.createStatement();
+        ResultSet rs = hsqldbStmt.executeQuery("SELECT CAST(DTMFIELD AS VARCHAR(26)) AS str FROM TABLE1 WHERE ID=1");
         rs.next();
         assertEquals("2017-03-26 02:00:00.000000", rs.getString(1));
+
+        /*
+         * also ensure that 02:00:00 -> 01:00:00 doesn't happen when writing back to Access
+         */
+        Statement ucaStmt = ucanaccess.createStatement();
+        ucaStmt.executeUpdate("UPDATE Table1 SET txtField='updated' WHERE id=1");
+        //
+        TimeZone tzUtc = TimeZone.getTimeZone("UTC");
+        //
+        Calendar expectedBackFromAccess = Calendar.getInstance(tzUtc);
+        expectedBackFromAccess.clear();
+        expectedBackFromAccess.set(2017, 2, 26, 2, 0, 0);
+        //
+        Database db = ucanaccess.getDbIO();
+        TimeZone prevJackcessTimeZone = db.getTimeZone();
+        db.setTimeZone(tzUtc);
+        Table tbl = db.getTable("Table1");
+        Row r = CursorBuilder.findRowByPrimaryKey(tbl, 1);
+        assertEquals(expectedBackFromAccess.getTimeInMillis(), r.getDate("dtmField").getTime());
+        //
+        db.setTimeZone(prevJackcessTimeZone);
     }
 
 }
