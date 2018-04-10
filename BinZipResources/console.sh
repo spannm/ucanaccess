e241bef081f8aedef0e5513b5faa475d0428660c
Gord Thompson <gordonthompson@users.sf.net>
2018-04-10 07:08:23 -0600

    4.0.4 release

--- BinZipResources/console.sh
+++ BinZipResources/console.sh
@@ -2,7 +2,7 @@
 UCANACCESS_HOME=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 echo $UCANACCESS_HOME
 
-CLASSPATH="$UCANACCESS_HOME/lib/hsqldb.jar:$UCANACCESS_HOME/lib/jackcess-2.1.6.jar:$UCANACCESS_HOME/lib/commons-lang-2.6.jar:$UCANACCESS_HOME/lib/commons-logging-1.1.1.jar:$UCANACCESS_HOME/ucanaccess-4.0.2.jar"
+CLASSPATH="$UCANACCESS_HOME/lib/hsqldb.jar:$UCANACCESS_HOME/lib/jackcess-2.1.11.jar:$UCANACCESS_HOME/lib/commons-lang-2.6.jar:$UCANACCESS_HOME/lib/commons-logging-1.1.3.jar:$UCANACCESS_HOME/ucanaccess-4.0.4.jar"
 
 if [ -d "$JAVA_HOME" -a -x "$JAVA_HOME/bin/java" ]; then
 	JAVACMD="$JAVA_HOME/bin/java"
