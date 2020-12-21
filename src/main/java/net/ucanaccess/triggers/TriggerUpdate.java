45261c327ac0586627f78cbb456b2719ccfffebd
jamadei <amadei.mar@gmail.com>
2020-12-21 19:25:03 +0100

    Blob bug in update

--- src/main/java/net/ucanaccess/triggers/TriggerUpdate.java
+++ src/main/java/net/ucanaccess/triggers/TriggerUpdate.java
@@ -17,46 +17,71 @@ package net.ucanaccess.triggers;
 
 import java.util.Map;
 
+import org.hsqldb.SessionInterface;
+import org.hsqldb.jdbc.JDBCConnection;
+import org.hsqldb.types.BlobData;
+
 import net.ucanaccess.commands.UpdateCommand;
+import net.ucanaccess.jdbc.BlobKey;
 import net.ucanaccess.jdbc.UcanaccessConnection;
+import net.ucanaccess.jdbc.UcanaccessSQLException;
 
 import com.healthmarketscience.jackcess.Table;
 
 public class TriggerUpdate extends TriggerBase {
-    @Override
-    public void fire(int type, String name, String tableName, Object[] oldR, Object[] newR) {
-        checkContext();
-        UcanaccessConnection conn = UcanaccessConnection.getCtxConnection();
-        if (conn.isFeedbackState()) {
-            return;
-        }
-        String execId = UcanaccessConnection.getCtxExcId();
-        try {
-            Table t = getTable(tableName, conn);
-            super.convertRowTypes(oldR, t);
-            super.convertRowTypes(newR, t);
-            if (valuesChanged(oldR, newR)) {
-                Map<String, Object> rowPattern = getRowPattern(oldR, t);
-                UpdateCommand c4j = new UpdateCommand(t, rowPattern, newR, execId);
-                conn.add(c4j);
-            }
-        } catch (Exception e) {
-            throw new TriggerException(e.getMessage());
-        }
-    }
+	@Override
+	public void fire(int type, String name, String tableName, Object[] oldR, Object[] newR) {
+		checkContext();
+		UcanaccessConnection conn = UcanaccessConnection.getCtxConnection();
+		if (conn.isFeedbackState()) {
+			return;
+		}
+		String execId = UcanaccessConnection.getCtxExcId();
+		try {
+			Table t = getTable(tableName, conn);
+			getBlobs(oldR);
+			getBlobs(newR);
+			super.convertRowTypes(oldR, t);
+			super.convertRowTypes(newR, t);
+			if (valuesChanged(oldR, newR)) {
+				Map<String, Object> rowPattern = getRowPattern(oldR, t);
+				UpdateCommand c4j = new UpdateCommand(t, rowPattern, newR, execId);
+				conn.add(c4j);
+			}
+		} catch (Exception e) {
+			throw new TriggerException(e.getMessage());
+		}
+	}
 
-    public boolean valuesChanged(Object[] oldR, Object[] newR) {
-        if (oldR.length != newR.length) {
-            return true;
-        }
-        for (int i = 0; i < oldR.length; ++i) {
-            if (oldR[i] == null ^ newR[i] == null) {
-                return true;
-            }
-            if (oldR[i] != null && newR[i] != null && !oldR[i].equals(newR[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean valuesChanged(Object[] oldR, Object[] newR) {
+		if (oldR.length != newR.length) {
+			return true;
+		}
+		for (int i = 0; i < oldR.length; ++i) {
+			if (oldR[i] == null ^ newR[i] == null) {
+				return true;
+			}
+			if (oldR[i] != null && newR[i] != null && !oldR[i].equals(newR[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void getBlobs(Object[] values) throws UcanaccessSQLException {
+		for (int i = 0; i < values.length; ++i) {
+			Object value = values[i];
+			if (value instanceof BlobData) {
+				BlobData bd = (BlobData) value;
+				JDBCConnection hsqlConn = (JDBCConnection) UcanaccessConnection.getCtxConnection()
+						.getHSQLDBConnection();
+				SessionInterface si = hsqlConn.getSession();
+				long length = bd.length(si);
+				byte[] bt = (byte[]) ((BlobData) value).getBytes(si, 0, (int) length);
+				values[i] = (bt.length > 0)
+						? BlobKey.getBlobKey(bt).getOleBlob(UcanaccessConnection.getCtxConnection().getDbIO())
+						: bt;
+			}
+		}
+	}
 }
