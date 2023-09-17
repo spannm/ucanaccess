93007ba3685fca0053b0433b33649ac5294a0e62
Markus Spann <spannm@outlook.de>
2023-09-17 09:53:32 +0200

    net-sf-ucanaccess-fork: Review random unit test failure

--- src/test/java/net/ucanaccess/test/integration/CounterTest.java
+++ src/test/java/net/ucanaccess/test/integration/CounterTest.java
@@ -11,12 +11,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 class CounterTest extends UcanaccessTestBase {
-    private String tableName = "T_BBB";
+    private String tableName = "t_bbb";
 
     @Override
     protected void init(AccessVersion _accessVersion) throws SQLException {
         super.init(_accessVersion);
-        executeStatements("CREATE TABLE " + tableName + " (Z COUNTER PRIMARY KEY, B char(4), C blob, d TEXT)");
+        executeStatements("CREATE TABLE " + tableName + " (z COUNTER PRIMARY KEY, B CHAR(4), c BLOB, d TEXT)");
     }
 
     @AfterEach
@@ -30,29 +30,32 @@ class CounterTest extends UcanaccessTestBase {
         init(_accessVersion);
         try (Statement st = ucanaccess.createStatement()) {
             st.execute("DISABLE AUTOINCREMENT ON " + tableName);
-            st.execute("INSERT INTO " + tableName + " (Z,B,C,D) VALUES (3,'C',NULL,NULL)"); // insert arbitrary
-                                                                                            // AutoNumber value
+            // insert arbitrary AutoNumber value
+            st.execute("INSERT INTO " + tableName + " (z, b, c, d) VALUES (3, 'C', NULL, NULL)");
 
             st.execute("ENABLE AUTOINCREMENT ON " + tableName);
 
-            st.execute("INSERT INTO " + tableName + " (B,C,D) VALUES ('D',NULL,NULL)"); // 4 (verify AutoNumber seed
-                                                                                        // updated)
+            // 4 (verify AutoNumber seed updated)
+            st.execute("INSERT INTO " + tableName + " (b, c, d) VALUES ('D', NULL, NULL)");
 
-            st.execute("INSERT INTO " + tableName + " (B,C,D) VALUES ('E' ,NULL,NULL)"); // 5
-            st.execute("INSERT INTO " + tableName + " (B,C,D) VALUES ('F',NULL,NULL)"); // 6
+            st.execute("INSERT INTO " + tableName + " (b, c, d) VALUES ('E', NULL, NULL)"); // 5
+            st.execute("INSERT INTO " + tableName + " (b, c, d) VALUES ('F', NULL, NULL)"); // 6
 
             st.execute("DISABLE AUTOINCREMENT ON " + tableName);
-            st.execute("INSERT INTO " + tableName + " (Z,B,C,D) VALUES (8,'H',NULL,NULL)"); // arbitrary, new seed = 9
-            st.execute("INSERT INTO " + tableName + " (Z,B,C,D) VALUES (7,'G',NULL,NULL)"); // arbitrary smaller than
-                                                                                            // current seed
-            st.execute("INSERT INTO " + tableName + " (Z,B,C,D) VALUES (-1,'A',NULL,NULL)"); // arbitrary negative value
+            st.execute("INSERT INTO " + tableName + " (z, b, c, d) VALUES (8, 'H', NULL, NULL)"); // arbitrary, new seed = 9
+            st.execute("INSERT INTO " + tableName + " (z, b, c, d) VALUES (7, 'G', NULL, NULL)"); // arbitrary smaller than current seed
+            st.execute("INSERT INTO " + tableName + " (z, b, c, d) VALUES (-1, 'A', NULL, NULL)"); // arbitrary negative value
             st.execute("ENABLE AUTOINCREMENT ON " + tableName);
-            st.execute("INSERT INTO " + tableName + " (B,C,D) VALUES ('I',NULL,NULL)"); // 9
-            Object[][] ver =
-                {{-1, "A", null, null}, {3, "C", null, null}, {4, "D", null, null}, {5, "E", null, null}, {6, "F", null, null}, {7, "G", null, null}, {8, "H", null, null}, {9, "I", null, null}};
-            dumpQueryResult("SELECT * FROM " + tableName);
-            checkQuery("SELECT * FROM " + tableName + " ORDER BY Z", ver);
+            st.execute("INSERT INTO " + tableName + " (b, c, d) VALUES ('I', NULL, NULL)"); // 9
         }
+
+        dumpQueryResult("SELECT * FROM " + tableName);
+        Object[][] ver =
+            {{-1, "A", null, null}, {3, "C", null, null}, {4, "D", null, null},
+            {5, "E", null, null}, {6, "F", null, null}, {7, "G", null, null},
+            {8, "H", null, null}, {9, "I", null, null}};
+        checkQuery("SELECT * FROM " + tableName + " ORDER BY z", ver);
+
     }
 
 }
