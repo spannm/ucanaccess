510a37d6cca4367fa536b06c13a2bec07ec39339
Markus S <spannm@outlook.de>
2023-11-24 13:58:53 +0100

    net-sf-ucanaccess-fork: Remove redundant imports

--- src/test/java/net/ucanaccess/jdbc/CsvDumpTest.java
+++ src/test/java/net/ucanaccess/jdbc/CsvDumpTest.java
@@ -11,7 +11,6 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * Integration test for {@link net.ucanaccess.console.Exporter}.
