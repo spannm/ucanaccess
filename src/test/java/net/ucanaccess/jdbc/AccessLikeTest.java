cb1415cf4e3ce349374d7af2edcdee24de5ab175
Markus Spann <spannm@outlook.de>
2023-11-27 15:03:47 +0100

    net-sf-ucanaccess-fork: Use sql constants

--- src/test/java/net/ucanaccess/jdbc/AccessLikeTest.java
+++ src/test/java/net/ucanaccess/jdbc/AccessLikeTest.java
@@ -1,7 +1,10 @@
 package net.ucanaccess.jdbc;
 
+import static net.ucanaccess.type.SqlConstants.*;
+
 import net.ucanaccess.test.UcanaccessBaseTest;
 import net.ucanaccess.type.AccessVersion;
+import net.ucanaccess.util.Sql;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -19,7 +22,7 @@ class AccessLikeTest extends UcanaccessBaseTest {
     @MethodSource("net.ucanaccess.type.AccessVersion#getDefaultAccessVersion()")
     void testLike(AccessVersion _accessVersion) throws SQLException {
         init(_accessVersion);
-        checkQuery("SELECT * FROM q_like2 ORDER BY campo2", singleRec("dd1"));
+        checkQuery(Sql.of(SELECT, "*", FROM, "q_like2", ORDER_BY, "campo2"), singleRec("dd1"));
     }
 
     @ParameterizedTest(name = "[{index}] {0}")
@@ -35,24 +38,24 @@ class AccessLikeTest extends UcanaccessBaseTest {
                 st.execute("INSERT INTO t_like3 (descr) VALUES('" + val + "')");
             }
 
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE 'a[*]a' ORDER BY ID",
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'a[*]a'", ORDER_BY, "ID"),
                 recs(rec("a*a"), rec("A*a")));
 
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE \"a*a\" AND '1'='1' AND (descr) like \"a*a\" ORDER BY ID",
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "\"a*a\" AND '1'='1' AND (descr) like \"a*a\"", ORDER_BY, "ID"),
                 recs(rec("aa"), rec("aBa"), rec("aBBBa"), rec("a*a"), rec("A*a")));
 
-            checkQuery("SELECT * FROM t_like3 WHERE descr LIKE 'a%a'",
+            checkQuery(Sql.of(SELECT, "*", FROM, "t_like3", WHERE, "descr", LIKE, "'a%a'"),
                 recs(rec(2, "aa"), rec(3, "aBa"), rec(4, "aBBBa"), rec(7, "a*a"), rec(8, "A*a")));
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE 'P[A-F]###'", singleRec("PB123"));
-            checkQuery("SELECT descr FROM t_like3 WHERE (t_like3.descr\n) \nLIKE 'P[!A-F]###' AND '1'='1'", singleRec("PZ123"));
-            checkQuery("SELECT * FROM t_like3 WHERE descr='aba'", singleRec(3, "aBa"));
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE '13[1-4][A-F]'", singleRec("132B"));
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE '13[!1-4]'", singleRec("138"));
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE '%s[#]%'", singleRec("ss#sss"));
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE '###'", singleRec("138"));
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE '###[#]'", singleRec("138#"));
-            checkQuery("SELECT descr FROM t_like3 WHERE descr LIKE '###[#]'", singleRec("138#"));
-            checkQuery("SELECT descr FROM t_like3 WHERE ((descr LIKE '###[#]'))", singleRec("138#"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'P[A-F]###'"), singleRec("PB123"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "(t_like3.descr\n) \n", LIKE, "'P[!A-F]###' AND '1'='1'"), singleRec("PZ123"));
+            checkQuery(Sql.of(SELECT, "*", FROM, "t_like3", WHERE, "descr='aba'"), singleRec(3, "aBa"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'13[1-4][A-F]'"), singleRec("132B"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'13[!1-4]'"), singleRec("138"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'%s[#]%'"), singleRec("ss#sss"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###'"), singleRec("138"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
             st.execute("DROP TABLE t_like3");
         }
     }
@@ -68,7 +71,7 @@ class AccessLikeTest extends UcanaccessBaseTest {
             st.execute("INSERT INTO t_like4(descr) VALUES('t11114')");
             st.execute("INSERT INTO t_like4(descr) VALUES('t1111C')");
             st.execute("INSERT INTO t_like4(descr) VALUES('t1111')");
-            checkQuery("SELECT DESCR FROM t_like4 WHERE descr NOT LIKE \"t#####\" ORDER BY id",
+            checkQuery(Sql.of(SELECT, "DESCR", FROM, "t_like4", WHERE, "descr NOT", LIKE, "\"t#####\"", ORDER_BY, "id"),
                 recs(rec("t1111C"), rec("t1111")));
             st.execute("DROP TABLE t_like4");
         }
