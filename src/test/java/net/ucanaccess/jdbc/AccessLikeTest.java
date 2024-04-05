9b5975d1687a66c46cea344fec386c800674006b
Markus Spann <spannm@outlook.de>
2024-04-05 14:50:41 +0200

    Extract table name

--- src/test/java/net/ucanaccess/jdbc/AccessLikeTest.java
+++ src/test/java/net/ucanaccess/jdbc/AccessLikeTest.java
@@ -30,33 +30,34 @@ class AccessLikeTest extends UcanaccessBaseTest {
     void testLikeExternal(AccessVersion _accessVersion) throws SQLException {
         init(_accessVersion);
 
+        String tbl = "t_like3";
         try (UcanaccessStatement st = ucanaccess.createStatement()) {
-            st.executeUpdate("CREATE TABLE t_like3 (id COUNTER PRIMARY KEY, descr MEMO)");
+            st.executeUpdate("CREATE TABLE " + tbl + " (id COUNTER PRIMARY KEY, descr MEMO)");
 
             for (String val : List.of(
                 "dsdsds", "aa", "aBa", "aBBBa", "PB123", "PZ123", "a*a", "A*a", "ss#sss", "*", "132B", "138", "138#")) {
-                st.execute("INSERT INTO t_like3 (descr) VALUES('" + val + "')");
+                st.execute("INSERT INTO " + tbl + " (descr) VALUES('" + val + "')");
             }
 
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'a[*]a'", ORDER_BY, "ID"),
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'a[*]a'", ORDER_BY, "ID"),
                 recs(rec("a*a"), rec("A*a")));
 
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "\"a*a\" AND '1'='1' AND (descr) like \"a*a\"", ORDER_BY, "ID"),
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "\"a*a\" AND '1'='1' AND (descr) like \"a*a\"", ORDER_BY, "ID"),
                 recs(rec("aa"), rec("aBa"), rec("aBBBa"), rec("a*a"), rec("A*a")));
 
-            checkQuery(Sql.of(SELECT, "*", FROM, "t_like3", WHERE, "descr", LIKE, "'a%a'"),
+            checkQuery(Sql.of(SELECT, "*", FROM, tbl, WHERE, "descr", LIKE, "'a%a'"),
                 recs(rec(2, "aa"), rec(3, "aBa"), rec(4, "aBBBa"), rec(7, "a*a"), rec(8, "A*a")));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'P[A-F]###'"), singleRec("PB123"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "(t_like3.descr\n) \n", LIKE, "'P[!A-F]###' AND '1'='1'"), singleRec("PZ123"));
-            checkQuery(Sql.of(SELECT, "*", FROM, "t_like3", WHERE, "descr='aba'"), singleRec(3, "aBa"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'13[1-4][A-F]'"), singleRec("132B"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'13[!1-4]'"), singleRec("138"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'%s[#]%'"), singleRec("ss#sss"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###'"), singleRec("138"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
-            checkQuery(Sql.of(SELECT, "descr", FROM, "t_like3", WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
-            st.execute("DROP TABLE t_like3");
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'P[A-F]###'"), singleRec("PB123"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "(" + tbl + ".descr\n) \n", LIKE, "'P[!A-F]###' AND '1'='1'"), singleRec("PZ123"));
+            checkQuery(Sql.of(SELECT, "*", FROM, tbl, WHERE, "descr='aba'"), singleRec(3, "aBa"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'13[1-4][A-F]'"), singleRec("132B"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'13[!1-4]'"), singleRec("138"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'%s[#]%'"), singleRec("ss#sss"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'###'"), singleRec("138"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
+            checkQuery(Sql.of(SELECT, "descr", FROM, tbl, WHERE, "descr", LIKE, "'###[#]'"), singleRec("138#"));
+            st.execute("DROP TABLE " + tbl);
         }
     }
 
