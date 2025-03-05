543ee030724ff533bf5368da433069f26b2170c9
rtyuy <rtyuy@foxmail.com>
2025-03-05 20:52:36 +0800

    Add the Connection property charset to support setting the encoding from the URL. For example, the encoding of early MDB files was not fixed and depended on the encoding used when they were created. When reading older MDB files and the encoding does not match the system's default encoding, it can cause garbled text issues. Therefore, adding an encoding property makes it convenient to set the encoding, such as jdbc:ucanaccess://d:/test.mdb;CHARSET=GBK

--- src/main/java/net/ucanaccess/jdbc/IJackcessOpenerInterface.java
+++ src/main/java/net/ucanaccess/jdbc/IJackcessOpenerInterface.java
@@ -4,8 +4,9 @@ import io.github.spannm.jackcess.Database;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public interface IJackcessOpenerInterface {
-    Database open(File fl, String pwd) throws IOException;
+    Database open(File fl, String pwd, Charset charset) throws IOException;
 
 }
