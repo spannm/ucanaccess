be64629ecb25f8d7a0094322309d26a46cb36fe0
Markus Spann <spannm@outlook.de>
2025-07-25 12:26:53 +0200

    Remove unneeded imports

--- src/test/java/net/ucanaccess/jdbc/CharsetGBKTest.java
+++ src/test/java/net/ucanaccess/jdbc/CharsetGBKTest.java
@@ -7,23 +7,21 @@ import org.junit.jupiter.api.Test;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class CharsetGBKTest extends UcanaccessBaseFileTest {
 
     @Test
     void testCharsetGBK() throws Exception {
         init();
+
         ucanaccess = buildConnection().withDbPath(getAccessTempPath()).withProp(Metadata.Property.charset, "GBK").build();
         String testSql ="SELECT * FROM table1";
         dumpQueryResult(testSql);
+
         try (PreparedStatement ps = ucanaccess.prepareStatement(testSql);
              ResultSet rs = ps.executeQuery()) {
             rs.next();
             assertEquals("GBK编码", rs.getString(2));
         }
-        //checkQuery(testSql);
 
     }
 
